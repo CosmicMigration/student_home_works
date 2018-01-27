8afe121695420944ae07b67de892a2d956f20fb8
Alex-Ilin <master.iljin97@gmail.com>
2018-01-27 19:23:34 +0200

    Task 3 and other

--- alex_ilin/src/hw_26_01_18/Main.java
+++ alex_ilin/src/hw_26_01_18/Main.java
@@ -2,24 +2,39 @@ package hw_26_01_18;
 
 import hw_26_01_18.task_1.Adam;
 import hw_26_01_18.task_1.Eva;
-
+import hw_26_01_18.task_2.AppleIPhone;
+import hw_26_01_18.task_2.SamsungGalaxyS2;
 
 
 public class Main {
     public static void main(String[] args) {
-        Adam adam = new Adam();
-        Eva eva = new Eva();
+//        Adam adam = new Adam();
+//        Eva eva = new Eva();
+//
+//        adam.setName("Adam");
+//        adam.setAge(25);
+//
+//        eva.setDress(true);
+//        eva.setAge(23);
+//        eva.setName("Eva");
+//
+//        System.out.println(adam.toString());
+//        System.out.println(eva.toString());
 
-        adam.setName("Adam");
-        adam.setAge(25);
+        AppleIPhone iPhone = new AppleIPhone();
+        SamsungGalaxyS2 sumsungGalaxy = new SamsungGalaxyS2();
 
-        eva.setDress(true);
-        eva.setAge(23);
-        eva.setName("Eva");
+        iPhone.setCompany("Apple Inc.");
+        iPhone.setCost(23000);
+        iPhone.setDeveloper("USA");
 
-        System.out.println(adam.toString());
-        System.out.println(eva.toString());
+        sumsungGalaxy.setPortForHeadPhones(true);
+        sumsungGalaxy.setCompany("Samnung");
+        sumsungGalaxy.setCost(12000);
+        sumsungGalaxy.setDeveloper("Korea");
 
+        System.out.println(iPhone.toString());
+        System.out.println(sumsungGalaxy.toString());
     }
 
 
