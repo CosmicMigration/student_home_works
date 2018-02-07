2d5eb93a52391c875091814d14f1fb44d3ab5081
kate <kategavua@gmail.com>
2018-02-07 13:06:14 +0200

    New branch

--- Student.java
+++
@@ -1,9 +0,0 @@
-<<<<<<< .merge_file_a05824
-Pablic class Student{
-HomeWork home = HomeWork();
-=======
-public class Student{
-Homework home = new Homework();
->>>>>>> .merge_file_a04068
-
-}
--- tania_druzh/src/Student.java
+++
@@ -1,9 +0,0 @@
-import les7_19_01_18.HomeWork;
-
-public class Student
-{
-    private HomeWork homework;
-    
-    public Student(){}
-
-}
